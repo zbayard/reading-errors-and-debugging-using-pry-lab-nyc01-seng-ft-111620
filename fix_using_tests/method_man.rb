05c3c76d9567740a5bf3c3bcea27bf4033238338
Zach Bayard <zbayard@gmail.com>
2020-10-21 20:29:48 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -4,6 +4,6 @@ def start_game(player1, player2)
   "Hello #{player1} & #{playar2}"
 end
 
-def play_game(player2)
-  "#{player2} is better than #{player1}"
+def play_game(player1, player2)
+  "#{player1} is better than #{player2}"
 end
